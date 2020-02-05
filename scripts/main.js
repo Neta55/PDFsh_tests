dac8860d8ead760caef2c391d777257862736572
Ineta Priga <ineta.priga@gmail.com>
2020-02-06 01:44:48 +0200

    5

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log('Running main.js');
+
+function openVideo() {
+    document.getElementById("how-video").classList.remove("static-video");
+    document.getElementById("how-video").classList.add("play-video");
+}
+
